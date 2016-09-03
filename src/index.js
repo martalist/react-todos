a56d464518cfc0bbed2e9eef29fb422c176960b2
Alistair Martin <alistairmartin01@gmail.com>
2016-09-04 00:20:43 +0930

    Add initial state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import todos from './reducers';
+import { addTodo, toggleTodo } from './actions';
 import App from './components/App';
 import './index.css';
 
 let store = createStore(todos);
+
+// initial state
+(() => {
+  ["Do something", "Do something else"].forEach(task => {
+    store.dispatch(addTodo(task))
+  });
+  store.dispatch(toggleTodo(1));
+})();
 
 ReactDOM.render(
   <Provider store={store}>
