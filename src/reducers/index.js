54ff70ccb6476365d1c9748bf488063bfe78cb70
Alistair Martin <alistairmartin01@gmail.com>
2016-09-03 21:19:09 +0930

    Refactor reducer into two, to handle individual todos and the todos array independently

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,36 @@
+// handle changes to individual todos
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+          task: action.text,
+          completed: false,
+          id: action.id
+        }
+    case 'TOGGLE_TODO':
+      return Object.assign(
+            {},
+            state,
+            {completed: !state.completed}
+      );
+    default:
+      return state;
+  }
+}
+
+// handle changes to the todos array
 const todos = (state=[], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [ 
         ...state,
-        {
-          task: action.text,
-          completed: false,
-          id: action.id
-        }
-      ]
+        todo({}, action)
+      ];
     case 'DELETE_TODO':
       return state.filter(todo => action.id !== todo.id)
     case 'TOGGLE_TODO':
-      return state.map(
-        todo => action.id !== todo.id ? 
-          todo :
-          Object.assign(
-            {},
-            state[action.id],
-            {completed: !state[action.id].completed}
-          )
+      return state.map(t => 
+        action.id === t.id ? todo(t, action) : t
       );
     default:
       return state;
